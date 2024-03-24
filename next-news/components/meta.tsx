af42bf9bd2736427f38043aa07e945ce99ce0b77
Wenny Yustalim <wennyyustalim@gmail.com>
2024-03-23 18:44:47 -0700

    fix styling, abandon css module

--- next-news/components/meta.tsx
+++ next-news/components/meta.tsx
@@ -13,7 +13,7 @@ export default function Meta() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <link rel="shortcut icon" href="/static/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <style jsx global>{`
         body {
--- next-news/components/tweet.tsx
+++ next-news/components/tweet.tsx
@@ -6,24 +6,63 @@ const Tweet = ({ tweet }) => {
     const { fullname, username, content, timestamp } = tweet;
 
     return (
-        <div className={styles.tweet}>
-            <Image
-                className={styles.avatar}
-                src="/elon.jpeg"
-                width={50}
-                height={50}
-                alt="Picture of the author"
-            />
-            <div className={styles.tweetBody}>
-                <div className={styles.tweetHeader}>
-                    {fullname}
-                    <div className={styles.tweetUsername}>
-                        <strong>{username}</strong>
+        <div>
+            <div className="tweet">
+                <Image
+                    className={styles.avatar}
+                    src="/elon.jpeg"
+                    width={50}
+                    height={50}
+                    alt="Picture of the author"
+                />
+                <div className="tweet-body">
+                    <div className="tweet-header">
+                        {fullname}
+                        <div className="tweet-username">
+                            <strong>{username}</strong>
+                        </div>
+                        <span className="tweet-timestamp">&nbsp; &#8226; {timestamp}</span>
                     </div>
-                    <span className={styles.tweetTimestamp}>&nbsp; &#8226; {timestamp}</span>
+                    <div className="tweet-content">{content}</div>
                 </div>
-                <div className={styles.tweetContent}>{content}</div>
             </div>
+            <style jsx>{`
+                .tweet {
+                    display: flex;
+                    flex-direction: row;
+                    border: 1px solid #ccc;
+                    padding: 10px;
+                    margin-bottom: 10px;
+                    border-radius: 5px;
+                }
+
+                .avatar {
+                    border-radius: 50%;
+                    margin-right: 5px;
+                }
+
+                .tweet-header {
+                    display: flex;
+                    align-items: center;
+                    margin-bottom: 5px;
+                }
+
+                .tweet-username {
+                    font-weight: bold;
+                    margin-right: 5px;
+                    margin-left: 5px;
+                }
+
+                .tweet-timestamp {
+                    color: #666;
+                    font-size: smaller;
+                }
+
+                .tweet-content {
+                    white-space: pre-wrap;
+                    /* Preserve line breaks */
+                }
+            `}</style>
         </div>
     );
 };
