b04517d8520130a6d690e196be332ab0b8293990
Wenny Yustalim <wennyyustalim@gmail.com>
2024-03-24 10:59:22 -0700

    fetch data from backend

--- next-news/lib/get-tweets.ts
+++ next-news/lib/get-tweets.ts
@@ -1,12 +1,53 @@
 export default async function getTweets() {
-    const res = await fetch('https://api.example.com/...')
+    const res = await fetch('http://127.0.0.1:5000/api/retrieve?top_k=4')
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
-
+    console.log('RESULT')
+    console.log(res)
     if (!res.ok) {
         // This will activate the closest `error.js` Error Boundary
         throw new Error('Failed to fetch data')
     }
 
-    return res.json()
+    // const json = {
+    //     "0": {
+    //         "avatar_url": "https://pbs.twimg.com/profile_images/1711185897415180288/lgwajQTW_normal.jpg",
+    //         "content": "Are you dealing with impostor syndrome? Here are three signs to look for: https://t.co/No0W7eHiuV https://t.co/buHTyVJzkz",
+    //         "handle": "Forbes",
+    //         "id": "1771786543453094220",
+    //         "retrieval_score": 0.8338501453399658,
+    //         "timestamp": "Sat Nov 21 02:09:57 +0000 2009",
+    //         "username": "Forbes"
+    //     },
+    //     "1": {
+    //         "avatar_url": "https://pbs.twimg.com/profile_images/1711185897415180288/lgwajQTW_normal.jpg",
+    //         "content": "Are you dealing with impostor syndrome? Here are three signs to look for: https://t.co/dRlcqUUtGg https://t.co/c7nEfPL5m3",
+    //         "handle": "Forbes",
+    //         "id": "1771451841227771998",
+    //         "retrieval_score": 0.832545280456543,
+    //         "timestamp": "Sat Nov 21 02:09:57 +0000 2009",
+    //         "username": "Forbes"
+    //     },
+    //     "2": {
+    //         "avatar_url": "https://pbs.twimg.com/profile_images/1518391475687493633/Bb6zQKr8_normal.jpg",
+    //         "content": "Advanced modeling here. https://t.co/1Og1jItX69",
+    //         "handle": "amasad",
+    //         "id": "1771361201207095619",
+    //         "retrieval_score": 0.8311523199081421,
+    //         "timestamp": "Tue Jul 13 12:31:51 +0000 2010",
+    //         "username": "Amjad Masad"
+    //     },
+    //     "3": {
+    //         "avatar_url": "https://pbs.twimg.com/profile_images/1518391475687493633/Bb6zQKr8_normal.jpg",
+    //         "content": "Corporate AI drama is accelerating faster than AI itself. https://t.co/QO3nceE8f3",
+    //         "handle": "amasad",
+    //         "id": "1771382779298848775",
+    //         "retrieval_score": 0.8302334547042847,
+    //         "timestamp": "Tue Jul 13 12:31:51 +0000 2010",
+    //         "username": "Amjad Masad"
+    //     }
+    // };
+    // return json;
+    const result = res.json()
+    return result
 }
--- next-news/pages/twitter.tsx
+++ next-news/pages/twitter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Page from '../components/page'
 import Tweet from "../components/tweet"
 import getTweets from '../lib/get-tweets'
+import Image from 'next/image';
 
 const tweet = {
   fullname: "John Doe",
@@ -9,19 +10,78 @@ const tweet = {
   content: "Funding secured.",
   timestamp: "6h",
 };
+export async function getStaticProps() {
+  const res = await fetch(`http://127.0.0.1:5000/api/retrieve?top_k=4`); // Replace with your data source
+  const data = await res.json();
 
-export default function Twitter() {
-  // let tweets = getTweets();
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function Twitter({ data }) {
   return (
     <Page>
-      <Tweet tweet={tweet} />
-      <Tweet tweet={tweet} />
-      <Tweet tweet={tweet} />
-      <Tweet tweet={tweet} />
-      <Tweet tweet={tweet} />
-      <Tweet tweet={tweet} />
-      <Tweet tweet={tweet} />
-      <Tweet tweet={tweet} />
+      {
+        Object.keys(data).map((key) => {
+          return (
+            <div key={data[key].id}>
+              <div className="tweet">
+                <img width={50} height={50} src={data[key].avatar_url} alt={data[key].handle + " avatar"} />
+                <div className="tweet-body">
+                  <div className="tweet-header">
+                    {data[key].handle}
+                    <div className="tweet-username">
+                      <strong>{data[key].handle}</strong>
+                    </div>
+                    <span className="tweet-timestamp">&nbsp; &#8226; 6h</span>
+                  </div>
+                  <div className="tweet-content">{data[key].content}</div>
+                </div>
+              </div>
+              <style jsx>{`
+                .tweet {
+                    display: flex;
+                    flex-direction: row;
+                    border: 1px solid #ccc;
+                    padding: 10px;
+                    margin-bottom: 10px;
+                    border-radius: 5px;
+                }
+
+                img {
+                    border-radius: 50%;
+                    margin-right: 5px;
+                }
+
+                .tweet-header {
+                    display: flex;
+                    align-items: center;
+                    margin-bottom: 5px;
+                }
+
+                .tweet-username {
+                    font-weight: bold;
+                    margin-right: 5px;
+                    margin-left: 5px;
+                }
+
+                .tweet-timestamp {
+                    color: #666;
+                    font-size: smaller;
+                }
+
+                .tweet-content {
+                    white-space: pre-wrap;
+                    /* Preserve line breaks */
+                }
+            `}</style>
+            </div>
+          )
+        })
+      }
     </Page>
   )
 }
